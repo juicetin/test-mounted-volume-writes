d9a705aff35646021da6c4a009438c1dce9e5eda
Justin Ting <justin.ting@nullify.cloud>
2024-03-05 22:35:48 +1100

    create test-dir as part of test-script

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 docker build -t test-mounted-volume-writes .
 
+mkdir -p test-dir
 # These should work - not throw the error:
 # bash: line 1: /app/test.txt: Permission denied
 docker run --rm -v ./test-dir:/app test-mounted-volume-writes
